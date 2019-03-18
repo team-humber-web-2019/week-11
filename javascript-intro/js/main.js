0b158c70197caf665660d633e65c12c1d407fd2d
Panacci Rocco <panaccir@nj-00118-001.mediastudies.ca>
2019-03-18 11:32:05 -0400

    Some fun with variables

---
+++ javascript-intro/js/main.js
@@ -0,0 +1,34 @@
+console.log(`Hello World!`);
+// Single line comment
+/* Multi line comment */
+
+
+
+// VARIABLES
+// - Arithmetic operators: + - * / = ()
+        // % **
+        // PEDMAN (Parenthesis, Exponents, Division/Multiplication, Addition/Subtraction)
+// - Rules to naming
+//      - Should start with a small letter
+//      - Use camel case for mutiple words
+//          - my-balance >> myBalance or mybalance
+//          - background-color >> backgroundColor
+//      - Symbols are not allowed, other than: _ and $
+//      - Must not start with a number 
+//      - Must not use Javascript reserved keywords
+// - Variable types
+//      - Numbers
+//      - Strings
+// - String notation ` vs ' vs "
+// - "let" vs "const" vs "var"
+// - Template literals
+
+// Number
+let myBalance = 1000;
+// String
+let myName = `Tim Berners-Lee`;
+// let myName = 'Tim Berners-Lee';
+// let myName = "Tim Berners-Lee";
+
+//console.log('This is my friend\'s favourite class')
+//console.log("<div class=\"oops\">This a new dynamic element</div>")
