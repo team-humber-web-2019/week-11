2bc7fd804d95a643ba96f16216b6d9dad48a2493
Panacci Rocco <panaccir@nj-00118-001.mediastudies.ca>
2019-04-01 13:28:07 -0400

    Added data-story images to help explain the process

--- week-13/README.md
+++ week-13/README.md
@@ -1,31 +1,43 @@
 # Week 13 `Monday, April 1st, 2019`
 
-## Overview
+## Schedule
+
+### Morning
 * Review
     * Variables: var, let, const
     * Datatypes: Number, String, Objects
     * Arithmetic: + - * / % ** = ()
     * Template literals: `I have ${apples * 2} apples`
         * Old method: 'I have ' + (apples * 2) + ' apples'
-    * Object data: properties (and subproperties)
+    * Object data: properties (and sub-properties)
 * Questions
-* Arrays
-    * Push, loops + forEach loop, mapping and filtering
-* Conditions (In depth)
-* Functions
-* Events (Intro)
-* DOM functions
-* Repo workflow
-* Assignment talk
-* Week 16
+* LESSON: Arrays
+    * Structure, indexing
 
-
-## Schedule 
-
-### Morning
 ### Lunch
-* 11:30 to 12:30
+* 11:45 to 12:40
+
 ### Afternoon
+* LESSON: Functions
+    * Calling, input, return, default
+* LESSON: Arrays
+    * push(), map()
+* LESSON: Arrays
+    * forEach() loop, filter()
+* LESSON: Conditions & Logic
+    * If statements
+* IN CLASS ASSIGNMENT: Game
+* TO DISCUSS:
+    * Assignment talk
+    * Repo workflow
+    * Week 16
+* TAKE HOME MATERIAL ("ADVANCED" TOPICS):
+    * String functions
+    * Loops (counted)
+    * Conditions (ternary)
+    * Arrays (reduce, etc)
 
 
 ## Next Week:
+* Events (Intro)
+* DOM functions
--- week-13/instructor/grocery-list/js/main.js
+++ week-13/instructor/grocery-list/js/main.js
@@ -5,11 +5,50 @@ let item1qty = 10;
 let item1name = `Apples`;
 let item1department = `Produce`;
 
+let item1 = {
+    qty: 10,
+    name: `Apples`,
+    dept: `Produce`
+};
+
 // 2. Create three of them (item1, item2, item3) in total: Apple, Steak, Bread
 
+let item2 = { 
+    qty: 2, 
+    name: `Steaks`, 
+    dept: `Meat`
+};
+
+let item3 = {
+    qty: 4,
+    name: `Bread Loafs`,
+    dept: `Bakery`
+};
 
 // 3. Add the three item variables you define to an Array: shoppingList
+// let shoppingList = [
+//     item1,
+//     item2,
+//     item3
+// ];
+
+const shoppingList = [
+    { qty: 10, name: `Apples`, dept: `Produce` },       // 0
+    { qty: 2, name: `Steaks`, dept: `Meat` },           // 1
+    { qty: 4, name: `Bread Loafs`, dept: `Bakery` }     // 2
+];
 
 
-// 4. Create 3 <li> in HTML using Javascript
-console.log(`You need ${item1qty} ${item1name} from the ${item1department} department`);
+// 4. Create 3 <li> in HTML document using Javascript by changing the variables to reflect the new data structure you created, then assigning each of the three strings to the appropriate property of the designated element in HTML
+
+const eleItem1 = document.getElementById(`item1`);
+const eleItem2 = document.getElementById(`item2`);
+const eleItem3 = document.getElementById(`item3`);
+
+
+eleItem1.innerHTML = `You need ${shoppingList[0].qty} ${shoppingList[0].name} from the ${shoppingList[0].dept} department`;
+eleItem2.innerHTML = `You need ${shoppingList[1].qty} ${shoppingList[1].name} from the ${shoppingList[1].dept} department`;
+eleItem3.innerHTML = `You need ${shoppingList[2].qty} ${shoppingList[2].name} from the ${shoppingList[2].dept} department`;
+
+
+// Note for #4: There are better ways to complete that task. Think back to last week!
