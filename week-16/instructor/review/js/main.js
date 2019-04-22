9e923e4ff8502bf5fa16626c9c873fe974297d9a
Panacci Rocco <panaccir@nj-00118-001.mediastudies.ca>
2019-04-22 14:55:32 -0400

    Semi-complete. Connected categories and cart

--- week-16/instructor/review/js/main.js
+++ week-16/instructor/review/js/main.js
@@ -1,12 +1,38 @@
-// Array
-const allProducts = [
-  { name: `Computer`, price: 2000, img: `comp.png` },
-  { name: `Notebook`, price: 20, img: `notebook.png` },
-  { name: `Mouse`, price: 40, img: `mouse.png` },
-  { name: `Laptop`, price: 3000, img: `laptop.png` },
+// Products are now in product.js
+
+const shoppingCart = [
+  { productId: 1002, qty: 1 },
+  { productId: 1004, qty: 2 },
 ];
 
 const ulProducts = document.getElementById('products');
+const ulCart = document.getElementById('cart');
+
+// Add a listener to the whole Product list
+ulProducts.addEventListener(`click`, event => {
+
+  //if (!event.target.matches('button')) return;
+
+  // Grab the product id from the button, if one is not present, EVACUATE IMMEDIATELY!!!! (return)
+  let prodId = event.target.dataset.productid;
+  if (!prodId) return;
+
+  // Check if the id exists already, update it, else push a new one
+  let whichId = shoppingCart.findIndex( item => prodId == item.productId);
+
+  if (whichId >= 0) {
+    shoppingCart[whichId].qty += 1; // Update cart quantity by 1
+  } 
+  else {
+    shoppingCart.push({ productId: prodId, qty: 1 }); // Add a new cart item
+  }
+
+  printTheCart();
+
+});
+
+
+
 
 // FUNCTION: getProductAsHTML
 //    Takes a string and return an HTML formatted string of whatever was passed
@@ -14,17 +40,28 @@ const ulProducts = document.getElementById('products');
 // RETURN: String
 const getProductAsHTML = (prod) => {
 
+  const category = allCategories.find( cat => prod.catId == cat.id );
+
   return `
     <li class="product">
       <div><img src="img/${ prod.img }"></div>
       <div>
-        <div>${ prod.name }</div>
+        <div style="background-color: ${category.color}">${ prod.name }</div>
         <div>$ ${ prod.price }</div>
-        <div><button>Add to Cart</button></div>
+        <div><button data-productid="${ prod.id }">Add</button></div>
       </div>
     </li>`;
 }
 
+const getCartItemAsHTML = (item) => { 
+
+  // Search the allProducts array for the name of the item with id == item.productId
+  //const product = { id: 1002, name: `Notebook`, price: 20, img: `notebook.png` };
+  const product = allProducts.find( prod => prod.id == item.productId );
+
+  return `<li>${item.qty} ${product.name}: $ ${item.qty * product.price}</li>` 
+
+};
 
 
 const printAllProducts = () => {
@@ -32,9 +69,20 @@ const printAllProducts = () => {
   ulProducts.innerHTML = allProducts
     .filter( prod => prod.price > 0 )
     .map( getProductAsHTML )
-    .join('\n')
+    .join('\n');
 
 } 
 
+const printTheCart = () => {
+
+  ulCart.innerHTML = shoppingCart
+    .map( getCartItemAsHTML )
+    .join('\n');
+
+}
+
+
+
 // Does the first print of the available products
 printAllProducts();
+printTheCart();
---
+++ week-16/instructor/review/js/product.js
@@ -0,0 +1,14 @@
+// Array
+const allProducts = [
+  { id: 1001, name: `Computer`, price: 2000, img: `comp.png`, catId: 2001 }, 
+  { id: 1002, name: `Notebook`, price: 20, img: `notebook.png`, catId: 2003 },
+  { id: 1003, name: `Mouse`, price: 40, img: `mouse.png`, catId: 2001 },
+  { id: 1004, name: `Laptop`, price: 3000, img: `laptop.png`, catId: 2001 },
+];
+
+const allCategories = [
+  { id: 2001, cat: `hardware`, color: `rgb(163, 163, 225)` },
+  { id: 2002, cat: `software`, color: `rgb(208, 152, 128` },
+  { id: 2003, cat: `physical`, color: `rgb(107, 206, 200)` },
+  { id: 2004, cat: `services`, color: `rgb(173, 180, 95)` },
+];
